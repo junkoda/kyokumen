42f9d7ae991ccf4cfa5bed5fe13576599455ca4e
Jun Koda <jun.koda@brera.inaf.it>
2016-09-29 16:59:28 +0200

    kif2sfen.py working.

--- script/kif2sfen.py
+++ script/kif2sfen.py
@@ -1,18 +1,34 @@
-import sys
-import re
+#import re
 
 nrow = 9
 
-Piece = {'玉': 'k', '飛':'r', '角':'b', '金':'g', '銀':'s', '桂':'n', '香':'l', '歩':'p', '馬':'+b', '竜':'+r', 'と':'+p'}
+Turn = ['b', 'w']
+Koma = ['☗', '☖']
+Piece = {'玉': 'k', '飛':'r', '角':'b', '金':'g', '銀':'s', '桂':'n', '香':'l', '歩':'p', '竜':'+r', '馬':'+b', '圭':'+n', '杏':'+l', 'と':'+p'}
 PieceRank = {'o': 0, 'k': 1, 'r': 2, 'b': 3, 'g': 4, 's': 5, 'n':6, 'l':7, 'p':8 }
 NumKanji = {'一': 1, '二': 2, '三':3, '四':4, '五':5, '六':6, '七':7, '八':8, '九':9}
 NumZenkaku = {'１': 1, '２': 2, '３':3, '４':4, '５':5, '６':6, '７':7, '８':8, '９':9}
 
+def piece_rank(p):
+    return PieceRank[p]
+
+def piece_raw(p):
+    return p.lower()[-1]
+
 
 class Ban:
-    def __init__(self):
-        self.ban = [None]*nrow*nrow
-        self.hand = ['', '']
+    def __init__(self, teai):
+        self.hand = [[], []]
+        self.teban = 'b'
+        self.made = ''
+        self.n = 0
+        if teai:
+            self.ban = initial_ban(teai)
+        else:
+            self.ban = ['']*nrow*nrow
+
+        assert len(self.ban) == nrow*nrow
+
 
     def __getitem__(self, key):
         """Get piece on ban
@@ -23,38 +39,67 @@ class Ban:
         if isinstance(key, int):
             i = key
         if isinstance(key, tuple):
-            i = key[0]*nrow + key[1]
+            i = key[1]*nrow + key[0]
         assert 0 <= i and i < nrow*nrow
         return self.ban[i]
+
+    def __repr__(self):
+        return "Ban " + self.sfen
+
 
     def __setitem__(self, key, value):
         if isinstance(key, int):
             i = key
         if isinstance(key, tuple):
-            i = key[0]*nrow + key[1]
+            i = key[1]*nrow + key[0]
         assert 0 <= i and i < nrow*nrow
-        return self.ban[i]
+        self.ban[i]= value
 
-    def move(self, orig, dest, p):
+    def move(self, orig, dest, p, promote):
         """ Move piece p at corrdinate orig to coordinate dest
         Args:
             orig: i, (ix, iy), or None if piece in hand is used
             dest: i or (ix, iy)
         """
-        turn = (p.lower() == p)
+        turn = (p.lower() == p)  # 0: sente, 1: gote
+        self.n += 1
+        self.teban = Turn[not turn]
+
         if orig is None:
             self.hand[turn].remove(p.lower())
         else:
-            assert self.ban(orig) == p
-            self.ban[orig] = None
+            assert self[orig] == p
+            self[orig] = ''
 
-        p_dest = self.ban[dest]
+        p_dest = self[dest]
         if p_dest:
-            hand[turn].append(p_dest.lower())
-        self.ban[dest] = p
+            self.hand[turn].append(piece_raw(p_dest))
 
+        if promote:
+            self[dest] = '+' + p
+        else:
+            self[dest] = p
+
+        self.made = str(9 - dest[0]) + str(dest[1] + 1)
+
+
+    @property
     def sfen(self):
-        pass
+        """return SFEN representation"""
+        s = sfen_board(self.ban)
+        s += ' ' + self.teban + ' '
+
+        if self.hand[0] or self.hand[1]:
+            s += sfen_hand(self.hand[0]).upper()
+            s += sfen_hand(self.hand[1])
+        else:
+            s += '-'
+
+
+        s += ' ' + str(self.n)
+        
+        return s
+
 
 class Kif:
     """Kif file reader
@@ -62,13 +107,13 @@ class Kif:
     def __init__(self, filename):
         self.f = open(filename)
         self.header = read_header(self.f)
-        self.ban = initial_ban(self.header['手合割'])
-        self.r = re.compile('\w+') #([一二三四五六七八九])')
+        self.ban = Ban(self.header['手合割'])
+        #self.r = re.compile('\w+') #([一二三四五六七八九])')
         self.dest = None
 
     def __iter__(self):
         for line in self.f:
-            print(line)
+            #print(line)
             v = line.rstrip().split(' ')
             n = int(v[0])
             mv = v[1]
@@ -84,10 +129,10 @@ class Kif:
             else:
                 ix = 9 - NumZenkaku[mv[0]]
                 iy = NumKanji[mv[1]] - 1
-                if ix and iy:
-                    dest = (ix, iy)
-                else:
-                    print('Error: unable to parse', mv)
+                assert 0 <= ix and ix < 9
+                assert 0 <= iy and ix < 9
+                #print('Error: unable to parse', mv, ix, iy)
+                dest = (ix, iy)
                 i += 2
 
             # piece
@@ -115,11 +160,11 @@ class Kif:
                 orig = (ix, iy)
 
             #n = v[0]
-            print(n, dest, orig, p)
+            self.ban.move(orig, dest, p, promote)
 
             self.dest = dest
 
-            yield line
+            yield (n, Koma[not (n % 2)] + mv, self.ban)
 
 
 
@@ -127,12 +172,41 @@ def initial_ban(teai):
     ban = []
 
     if teai == '平手':
-        for p in enumerate('lnsgkgsnl r     b ppppppppp                  PPPPPPPPP B    R LNSGKGSNL'):
-            ban.append(p)
+        for p in 'lnsgkgsnl r     b ppppppppp                           PPPPPPPPP B     R LNSGKGSNL':
+            ban.append(p.strip())
     else:
         raise ValueError()
 
     return ban
+
+def sfen_board(ban):
+    """Convert ban (nrow*nrow array) to sfen string
+    """
+    s = ''
+    num = 0
+    for iy in range(nrow):
+        for ix in range(nrow):
+            i = iy*nrow + ix
+            if ban[i]:
+                if num:
+                    s += str(num)
+                    num = 0
+                s += ban[i]
+            else:
+                num += 1
+        if iy < 8:
+            if num:
+                s += str(num)
+                num = 0
+            s += '/'
+    return s
+
+def sfen_hand(hand):
+    hand.sort(key=piece_rank)
+    s=''
+    for h in hand:
+        s += h
+    return s
 
 def read_header(f):
     """Read header of kif file
@@ -155,8 +229,16 @@ def read_header(f):
 
     return header;
 
-for b in Kif('../data/test.dat'):
-    pass
+def main():
+    import sys
+    if len(sys.argv) == 1:
+        print('python3 kif2sfen filename.kif')
+        sys.exit(0)
 
-#ban = Ban()
-#ban.read('../data/test.dat')
+    filename = sys.argv[-1]
+    for i,mv,b in Kif(filename):
+        print('<span class="mv" data-made="' + b.made + '" data-sfen="' + b.sfen + '">' + mv + '</span>')
+
+if __name__ == "__main__":
+    main()
+
